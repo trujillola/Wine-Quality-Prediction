61638b8260a7c32cb87a02a746caec3422152c1d
trujillola <trujillola@eisti.eu>
2022-11-23 17:09:49 +0100

    problème d'import de modules

---
+++ app/model/exploration.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+from app.objects.wine import FileManager
+
+#sys.path.append('../objects')
+# sys.path.append('../')
+# from objects.wine import Wine, FileManager
+
+# from app.objects.wine import FileManager
+pd.set_option('display.max_column',13)
+
+# No missing values
+# Min quality value = 3
+# Max quality value = 8
+# Seulement 
+
+
+fm = FileManager("./app/data/Wines.csv")
+df = fm.read_data()
+print(df.columns)
+print(df.shape)
+print(df.head())
+print(df.describe())
+df['quality'].value_counts().hist()
+
